4ed0e671cab6501ab48fed4daa22388001a4e25b
Harrison Bohl <bohlharrison357@gmail.com>
2020-11-28 23:15:56 +1000

    still +messing around with randomfractals.py. Trying to generate curved stuff. Think this is about as far as turtle can go, probably need to start using the superiour method.

--- randomfractals.py
+++ randomfractals.py
@@ -1,32 +1,45 @@
 from turtle import *
+import math
 
 
-def fractal(length, iterations):
+def fractal1(length, iterations, sides):
     if iterations == 0:
         forward(length)
         return
     right(0)
-    fractal(length / 3, iterations - 1)
+    fractal1(length / 2, iterations - 1, sides)
     right(90)
-    fractal(length / 3, iterations - 1)
+    fractal1(length / 2, iterations - 1, sides)
     left(90)
-    fractal(length / 3, iterations - 1)
+    fractal1(length / 2, iterations - 1, sides)
 
 
-def draw_fractal(length, iterations):
+def fractal2(length, iterations, sides):
+    if iterations == 0:
+        forward(length)
+        return
+    for y in range(length):
+        forward(length - y)
+        left(length - y)
+        fractal2(length, iterations - 1, sides)
+
+
+def draw_fractal(length, iterations, sides, fractal):
+    speed(10)
     hideturtle()
     penup()
-    goto(-length / 3, (2 * length) / 3)
+    goto(50, 200)
+    showturtle()
     pendown()  # mr ryan moment
-    for i in range(4):
+    for i in range(sides):
         color("black")
-        fractal(length, iterations)
-        left(-90)
+        fractal(length, iterations, sides)
+        left(360 / sides)
     mainloop()
 
 
 def main():
-    draw_fractal(400, 4)
+    draw_fractal(50, 3, 16, fractal2)
 
 
 if __name__ == "__main__":
