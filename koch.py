0c7d22671aaba4e88449a123a14263ff6d2ad3f1
Harrison Bohl <bohlharrison357@gmail.com>
2020-11-28 20:05:48 +1000

    okay so basically this is kinda neato.

--- koch.py
+++ koch.py
@@ -1,17 +1,17 @@
 from turtle import *
 
 
-def koch_snowflake(iterations):
+def koch_snowflake(length, iterations):
     if iterations == 0:
-        forward(1)
+        forward(length)
         return
-    koch_snowflake(iterations - 1)
+    koch_snowflake(length / 3, iterations - 1)
     left(60)
-    koch_snowflake(iterations - 1)
+    koch_snowflake(length / 3, iterations - 1)
     right(120)
-    koch_snowflake(iterations - 1)
+    koch_snowflake(length / 3, iterations - 1)
     left(60)
-    koch_snowflake(iterations - 1)
+    koch_snowflake(length / 3, iterations - 1)
 
 
 def draw_snowflake():
@@ -19,11 +19,17 @@ def draw_snowflake():
     up()
     backward(200 / 2.0)
     down()
-    fillcolor("black")
-    begin_fill()
     for i in range(3):
         color("black")
-        koch_snowflake(4)
+        koch_snowflake(300, 4)
         left(-120)
     end_fill()
     mainloop()
+
+
+def main():
+    draw_snowflake()
+
+
+if __name__ == "__main__":
+    main()
